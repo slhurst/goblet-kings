3dbed33941f7fdddfb2e2b702684c6ca169250d3
Graeme-Miller <graemewmiller@btinternet.com>
2014-09-20 03:18:16 +0000

    occupancy check works

--- PiCamController.py
+++ PiCamController.py
@@ -6,6 +6,7 @@ from pygraph.classes.exceptions import AdditionError
 from xs_and_os_validator import validate_board
 
 import scipy.spatial.distance as spsd
+import math
 
 def takePicture():
     with picamera.PiCamera() as camera:
@@ -42,30 +43,59 @@ def getAndVerifyImage():
 def getRectanglesFromImage(image):
     return[blob for blob in image.findBlobs(minsize = 50, maxsize = 40000) if blob.isRectangle(0.5)]
 
-def getSpaces(image):
+def getNonRectangularBlobsFromImage(image):
+    return[blob for blob in image.invert().findBlobs(minsize = 10, maxsize = 20000)]
+
+def getSpaces(image):      
     BUFFER = 90
     g = graph()
+
     
+   
+    pieces = getNonRectangularBlobsFromImage(image)
     rectangles = getRectanglesFromImage(image)
     rectanglesToId = {}
+    rectanglesToFill = {}
     nextId = 1
-    for r in rectangles:
-        rectanglesToId[r] = str(nextId)
-        g.add_node(str(nextId))
-        nextId += 1
-    #print spsd.pdist(rectangles)
-    for rectangleOne in rectangles:
-##        image.show()
-        
 
+    #row, then y sorting
+    newRectangles = []
+    rectangles.sort(key=lambda rectum: rectum.x)
+    rows = math.floor(math.sqrt(len(rectangles)))
+    print(rows)
+    for (index, rectangle) in enumerate(rectangles):        
+        row = math.floor(index/rows)        
+        newRectangles.append((row, rectangle))
+    newRectangles.sort(key=lambda rectum: (rectum[0], rectum[1].y))
+    
+    for (row, r) in newRectangles:
+        #find overlapping
         drawingLayer = image.dl()
-        rectangleOne.draw(layer=drawingLayer)
+        r.draw(layer=drawingLayer)
+
+        overlapping = False
+        
+        for piece in pieces:            
+            if(r.overlaps(piece)):
+                overlapping = True        
+                piece.draw(layer=drawingLayer, color=(120,120,0))
 
         image.show()
-        time.sleep(0.25)
+        image.removeDrawingLayer()
+        #find overlapping end
+        
+        rectanglesToId[r] = str(nextId)
+        rectanglesToFill[r] = overlapping
+        g.add_node(str(nextId))
+        nextId += 1
+    for (row, rectangleOne) in newRectangles:       
+        #drawingLayer = image.dl()
+        #rectangleOne.draw(layer=drawingLayer)
+        #image.show()
+        #time.sleep(0.25)
         rectangleOneLongestSide = max(rectangleOne.width(), rectangleOne.height())
         
-        for rectangleTwo in rectangles:            
+        for (row, rectangleTwo) in newRectangles:            
             if(rectangleOne != rectangleTwo):                
                 rectangleTwoLongestSide = max(rectangleTwo.width(), rectangleTwo.height())
                 distanceBetweenRectangles = spsd.pdist([(rectangleOne.centroid()[0], rectangleOne.centroid()[1]), (rectangleTwo.centroid()[0], rectangleTwo.centroid()[1])])
@@ -74,21 +104,25 @@ def getSpaces(image):
                         g.add_edge((rectanglesToId[rectangleOne], rectanglesToId[rectangleTwo]))
                     except AdditionError:
                         pass
-                    rectangleTwo.draw(layer=drawingLayer, color=(0,0,128))                    
-                                       
+                    rectangleTwo.draw(layer=drawingLayer, color=(0,0,128))                                                        
                     print(rectangleOne, " ", rectangleTwo)
-##                    time.sleep(0.25)
-        image.show() 
-        image.removeDrawingLayer()
-    return g
 
-print "Welcome to GobletKingsGames, ",
-image = getAndVerifyImage()
-image = getImage()
+        #image.show() 
+        #image.removeDrawingLayer()
+    return (g, rectanglesToFill)
 
-g = getSpaces(image)
-if validate_board(g):
-    print "You've made a valid board"
-else:
-    print "Your board is invalid - try again!"
+def getGraph():
+    while(True):        
+        spaces = getSpaces(getImage())
+        if validate_board(spaces[0]):
+            print "You've made a valid board"
+            for space in spaces[1]:                
+                print(space, spaces[1][space])
+            time.sleep(200)
+            return spaces
+        else:
+            print "Your board is invalid - try again!"
+        
+
+getGraph()
 
